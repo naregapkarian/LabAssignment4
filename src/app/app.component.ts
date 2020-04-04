ad5a2c1aff86d9e82819160c9f181855ecf097bb
Nareg Apkarian <you@example.com>
2020-04-03 23:11:48 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LabAssignment3';
+
+    private firstName = 'Nareg';
+    private lastName = 'Apkarian';
+  
+
+    showGreeting() {
+        const row2 = document.getElementById("R1C2");
+        row2.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+}
+
+    resetGreeting() {
+        const row2 = document.getElementById("R1C2");
+        row2.innerHTML = "R1C2";
+    }
+
+
 }
